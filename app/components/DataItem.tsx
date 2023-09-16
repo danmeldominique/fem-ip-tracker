88542a738f6f4c2e01c73b25241602663b82184f
Daniel Dominique <danmeldominique@gmail.com>
2023-09-16 12:56:03 +1000

    fixes

--- app/components/DataItem.tsx
+++ app/components/DataItem.tsx
@@ -7,9 +7,9 @@ type DataItemProps = {
 
 export default function DataItem({ title, value }: DataItemProps) {
   return (
-    <div className="data-item sm:flex-1">
-    <h4 className="data-item-title">{title}</h4>
-    <p className="data-item-value">{value}</p>
+    <div className="sm:flex-1 text-center sm:text-left px-4">
+    <h4 className="text-[0.6rem] sm:font-semibold tracking-widest text-gray-500">{title}</h4>
+    <p className="text-[1.2rem] sm:text-[1rem] font-semibold text-gray-800">{value}</p>
   </div>
   )
 }
--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction } from "@remix-run/node";
-import styles from './global.css'
+import styles from './tailwind.css'
 import "leaflet/dist/leaflet.css";
 
 import {
@@ -15,10 +15,7 @@ import { cssBundleHref } from "@remix-run/css-bundle";
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref },
   {rel: "stylesheet", href: styles },
-  {rel: "stylesheet", href: "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css", integrity:"sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="}] : [
-  {rel: "stylesheet", href: styles },
-  {rel: "stylesheet", href: "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css", integrity:"sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="}
-  ])
+  {rel: "stylesheet", href: "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css", integrity:"sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="}] : [])
 ] ;
 
 export default function App() {
@@ -30,7 +27,6 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
-
       </head>
       <body className="font-sans">
         <Outlet />
